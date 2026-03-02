33561874a1d82ece136e754bc9622cd931193f39
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2026-03-02 23:00:00 +0530

    Updates

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import NextAuth from "next-auth";
 import bcrypt from "bcrypt";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
