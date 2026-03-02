3c39d7b7cdb2fd8712831776352c76db96b1f4ab
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2026-03-02 23:42:46 +0530

    Updated

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import "./globals.css";
+import { usePathname } from "next/navigation";
+import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { Toaster } from "react-hot-toast";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const showNavbar = !pathname.startsWith("/auth/");
+
+  let pageTitle = "FC - FOSTIIMA Chapter";
+
+  if (pathname !== "/") {
+    const formatted = pathname
+      .replace("/", "")
+      .split("/")
+      .map((p) => p.charAt(0).toUpperCase() + p.slice(1))
+      .join(" / ");
+
+    pageTitle = `${formatted} - FC - FOSTIIMA Chapter`;
+  }
+
+  useEffect(() => {
+    document.title = pageTitle;
+  }, [pageTitle]);
+
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <SessionProvider>
+          {showNavbar && <Navbar />}
+          <Toaster position="top-right" />
+          {children}
+        </SessionProvider>
+      </body>
+    </html>
+  );
+}
